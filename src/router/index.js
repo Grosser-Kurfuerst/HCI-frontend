e2302d3f81ceba65596cc30160fbced5f6ba49ba
86150 <2041733900@qq.com>
2023-12-30 16:04:28 +0800

    路由跳转返回顶部

--- src/router/index.js
+++ src/router/index.js
@@ -93,6 +93,9 @@ VueRouter.prototype.push = function push(location) {
 
 const router = new VueRouter({
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  },
 });
 
 export default router;
